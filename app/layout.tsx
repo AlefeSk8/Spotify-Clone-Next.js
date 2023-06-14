77f6aac675ff6b91bc0d18a577b8f54da4b7cb70
Álefe Araujo <alefe_sk8@hotmail.com>
2023-06-14 00:27:30 -0300

    First commit of my Spotify clone project

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import Sidebar from '@/components/Sidebar'
+import './globals.css'
+import { Figtree } from 'next/font/google'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Spotify Clone',
+  description: 'Listen to music!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Sidebar>
+          {children}
+        </Sidebar>
+      </body>
+    </html>
+  )
+}
