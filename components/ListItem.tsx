8d81244dd15eaf11348680e4d23a8c670fb153c3
Álefe Araujo <alefe_sk8@hotmail.com>
2023-06-14 01:20:12 -0300

    Added liked songs button

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
 
 interface ListItemProps {
   image: string;
@@ -17,7 +18,10 @@ export function ListItem({ image, name, href }: ListItemProps) {
   }
 
   return (
-    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+    <button 
+      onClick={onClick}
+      className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
+    >
       <div className="relative min-h-[64px] min-w-[64px]">
         <Image 
           className="object-cover"
@@ -29,6 +33,9 @@ export function ListItem({ image, name, href }: ListItemProps) {
       <p className="font-medium truncate py-5">
         {name}
       </p>
+      <div className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+        <FaPlay className="text-black" />
+      </div>
     </button>
   )
 }
