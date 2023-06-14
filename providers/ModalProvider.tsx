1e389568a2f45cbdf195dc16fe1969325aab5f11
Álefe Araujo <alefe_sk8@hotmail.com>
2023-06-14 20:52:51 -0300

    Configured Supabase and Stripe

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { Modal } from "@/components/Modal"
+import { useEffect, useState } from "react"
+
+export function ModalProvider() {
+  const [isMounted, setIsMounted] = useState(false)
+  
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+  
+  if (!isMounted) {
+    return null
+  }
+  
+  return (
+    <>
+      <Modal />
+    </>
+  )
+}
