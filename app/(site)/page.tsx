bc171fe8a4b7815964ccc2400e6215592ddfb39e
Álefe Araujo <alefe_sk8@hotmail.com>
2023-06-14 01:20:12 -0300

    Added liked songs button

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -18,7 +18,16 @@ export default function Home() {
           </div>
         </div>
       </Header>
-      
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold ">
+            Newest songs
+          </h1>
+        </div>
+        <div className="">
+          List of Songs!
+        </div>
+      </div>
     </div>
   )
 }
