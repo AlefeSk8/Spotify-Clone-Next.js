20f27f05d70848b80b6a17bc2fd53ea42f168f6c
Álefe Araujo <alefe_sk8@hotmail.com>
2023-06-14 01:03:15 -0300

    Developing the visual parts

---
+++ components/ListItem.tsx
@@ -0,0 +1,34 @@
+'use client'
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface ListItemProps {
+  image: string;
+  name: string;
+  href: string;
+}
+
+export function ListItem({ image, name, href }: ListItemProps) {
+  const router = useRouter()
+
+  const onClick = () => {
+    // Add authentication before push
+    router.push(href)
+  }
+
+  return (
+    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+      <div className="relative min-h-[64px] min-w-[64px]">
+        <Image 
+          className="object-cover"
+          fill
+          src={image}
+          alt="Image"
+        />
+      </div>
+      <p className="font-medium truncate py-5">
+        {name}
+      </p>
+    </button>
+  )
+}
